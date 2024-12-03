47a1242689fc48aa5f0304593c16187fd6ba0824
Tarun <Tarun@CSPC312>
2024-12-03 18:11:36 +0530

    fixes

--- store/urls.py
+++ store/urls.py
@@ -9,6 +9,11 @@ urlpatterns = [
     path('park/create/', views.create_park_view, name='create_park_view'),
     path('park/edit/<int:id>/', views.edit_park_view, name='edit_park_view'),
     path('park/delete/<int:id>/', views.delete_park_view, name='delete_park_view'),
+    
+    path('trails/', TrailListView.as_view(), name='trail_list'),
+    path('trails/create/', TrailCreateView.as_view(), name='trail_create'),
+    path('trails/edit/<int:pk>/', TrailEditView.as_view(), name='trail_edit'),
+    path('trails/delete/<int:pk>/', TrailDeleteView.as_view(), name='trail_delete'),
 ]
 
 if settings.DEBUG:
